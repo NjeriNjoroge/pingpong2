83719d902aa085815b56b422b287a71da8dc9f54
NjeriNjoroge <wanjorogegrace@gmail.com>
2017-06-28 15:38:38 +0300

    created an env file also my api key works

---
+++ js/weather-interface.js
@@ -0,0 +1,13 @@
+var apiKey = "53d6f5347d3ec764119de99e4e6dd46b";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response){
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    }).fail(function(error) {
+      $('.showWeather').text(error.responseJSON.message);    });
+  });
+});
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -22,3 +22,16 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('#time').text(moment());
 });
+
+var apiKey = "53d6f5347d3ec764119de99e4e6dd46b";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+           $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    });
+  });
+});
